fa1425745d751c5be61183ab9291713796bcddf7
buhuiyonggou <yangli4944@gmail.com>
2023-12-01 18:12:14 -0800

    show spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,27 @@
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImgeUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, errors, isLoading } = useGenres();
 
+  if (errors) return null;
+  if (isLoading) return <Spinner />;
   return (
-    <ul>
-      {data.map((d) => (
-        <li key={d.id}>{d.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY={"5px"}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImgeUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
