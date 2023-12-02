a37c1640fcd8e106ff00fb16b02f0ef3de30deb5
buhuiyonggou <yangli4944@gmail.com>
2023-12-01 23:20:57 -0800

    build search input layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   Text,
   Show,
   HStack,
+  Flex,
 } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./components/NavBar";
@@ -50,20 +51,22 @@ function App() {
       </Show>
 
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-          <PlatformSelector
-            selectedPlatform={gameQuery.platform}
-            onSelectPlatform={(platform) =>
-              setGameQuery({ ...gameQuery, platform })
-            }
-          />
+        <Flex paddingLeft={2} marginBottom={5}>
+          <Box marginRight={5}>
+            <PlatformSelector
+              selectedPlatform={gameQuery.platform}
+              onSelectPlatform={(platform) =>
+                setGameQuery({ ...gameQuery, platform })
+              }
+            />
+          </Box>
           <SortSelector
             sortOrder={gameQuery.sortOrder}
             onSelectSortOrder={(sortOrder) =>
               setGameQuery({ ...gameQuery, sortOrder })
             }
           />
-        </HStack>
+        </Flex>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import MyAnimeList from "../assets/myanimelist.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding="10px">
+    <HStack padding="10px">
       <Image src={MyAnimeList} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
